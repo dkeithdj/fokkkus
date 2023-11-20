d649e5e4bc0d75596b267822dc996821429fb543
d0o-o0p <stllgalax@gmail.com>
2023-11-20 20:23:09 +0800

    first version of toggle, not final, have to find other ways pa atm

---
+++ lib/themedata.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Custom light theme
+ThemeData lightTheme = ThemeData(
+  primaryColor: Colors.blue,
+  hintColor: Colors.orange,
+  // Other properties like fontFamily, textTheme, etc.
+);
+
+// Custom dark theme
+ThemeData darkTheme = ThemeData(
+  primaryColor: Colors.teal,
+  hintColor: Colors.amber,
+  brightness: Brightness.dark,
+  // Other properties like fontFamily, textTheme, etc.
+);
--- lib/timertab.dart
+++ lib/timertab.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 
-class TimerTab extends StatelessWidget {
-  const TimerTab({super.key});
+class TimerTab extends StatefulWidget {
+  const TimerTab({Key? key}) : super(key: key);
+
+  @override
+  State<TimerTab> createState() => _TimerTabState();
+}
+
+class _TimerTabState extends State<TimerTab> {
+  bool _isDarkMode = false;
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Timer Page'),
+    return Theme(
+      data: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Color(0xFF2E232F),
+                ),
+                child: ToggleButtons(
+                  constraints: const BoxConstraints.expand(
+                    width: 25,
+                    height: 25,
+                  ),
+                  borderRadius: BorderRadius.circular(18.0),
+                  onPressed: (int index) {
+                    setState(() {
+                      _isDarkMode = index == 1;
+                    });
+                  },
+                  isSelected: [_isDarkMode, !_isDarkMode],
+                  selectedColor: Color(0xFF2E232F),
+                  fillColor: Colors.transparent,
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  children: const [
+                    Icon(
+                      Icons.wb_sunny,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                    Icon(
+                      Icons.nightlight_round,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
