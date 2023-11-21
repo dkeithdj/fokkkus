83b0b5a186a9089fa9fe937666274b6f65816f7a
d0o-o0p <stllgalax@gmail.com>
2023-11-22 00:04:09 +0800

    major changes in timertab, much better theme toggle

--- lib/timertab.dart
+++ lib/timertab.dart
@@ -8,58 +8,81 @@ class TimerTab extends StatefulWidget {
 }
 
 class _TimerTabState extends State<TimerTab> {
-  bool _isDarkMode = false;
+  bool _switch = false;
+  final ThemeData _dark = ThemeData(brightness: Brightness.dark);
+  final ThemeData _light = ThemeData(brightness: Brightness.light);
 
   @override
   Widget build(BuildContext context) {
     return Theme(
-      data: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      data: _switch ? _dark : _light,
       child: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Color(0xFF2E232F),
-                ),
-                child: ToggleButtons(
-                  constraints: const BoxConstraints.expand(
-                    width: 25,
-                    height: 25,
-                  ),
-                  borderRadius: BorderRadius.circular(18.0),
-                  onPressed: (int index) {
-                    setState(() {
-                      _isDarkMode = index == 1;
-                    });
-                  },
-                  isSelected: [_isDarkMode, !_isDarkMode],
-                  selectedColor: Color(0xFF2E232F),
-                  fillColor: Colors.transparent,
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  children: const [
-                    Icon(
-                      Icons.wb_sunny,
-                      color: Colors.white,
-                      size: 15,
+        // backgroundColor: const Color(0xFFFAF9FA),
+        backgroundColor: Colors.amberAccent,
+        body: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40, right: 50),
+                      child: Switch(
+                        value: _switch,
+                        onChanged: (_newvalue) {
+                          setState(
+                            () {
+                              _switch = _newvalue;
+                            },
+                          );
+                          const VisualDensity(horizontal: -2.0, vertical: -2.0);
+                        },
+                        activeColor: const Color(0xFF2E232F),
+                        inactiveTrackColor: const Color(0xFFFAF9FA),
+                        thumbColor: MaterialStateProperty.resolveWith<Color>(
+                            getThumbColor),
+                      ),
                     ),
-                    Icon(
-                      Icons.nightlight_round,
-                      color: Colors.white,
-                      size: 15,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50, right: 50),
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image:
+                                AssetImage('../lib/icons/pomodorosettings.png'),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50, right: 40),
+                      child: Container(
+                          width: 32,
+                          height: 32,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  '../lib/icons/rotate-smartphone.png'),
+                              fit: BoxFit.contain,
+                            ),
+                          )),
                     ),
                   ],
                 ),
-              ),
-            ],
-          ),
-        ),
+              ],
+            )),
       ),
     );
   }
 }
+
+Color getThumbColor(Set<MaterialState> states) {
+  if (states.contains(MaterialState.selected)) {
+    return Colors.black12; //switch on
+  }
+  return const Color(0xFF2E232F);
+}
+//
