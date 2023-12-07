b77f7639d692cb4a5ee8e2bda7a4c9f6cc016d9c
d0o-o0p <stllgalax@gmail.com>
2023-12-08 05:42:17 +0800

    added todotab

--- lib/tabs/todotab.dart
+++ lib/tabs/todotab.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:fokkkus/theme/themeprovider.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
-class ToDoTab extends StatelessWidget {
+class ToDoTab extends StatefulWidget {
   const ToDoTab({super.key});
 
   @override
+  State<ToDoTab> createState() => _ToDoTabState();
+}
+
+class _ToDoTabState extends State<ToDoTab> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('To-Do Page'),
+    ThemeData themeData = Provider.of<ThemeProvider>(context).themeData;
+    DateTime currentDate = DateTime.now();
+    String dayOfMonth = DateFormat('dd').format(currentDate);
+    String monthOfYear = DateFormat('MMMM').format(currentDate);
+    String year = DateFormat('yyyy').format(currentDate);
+    String dayOfWeek = DateFormat('EEEE').format(currentDate);
+
+    return Stack(
+      children: [
+        Column(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                const Spacer(),
+                Text(
+                  dayOfMonth,
+                  style: TextStyle(
+                    color: themeData.textTheme.titleLarge?.color,
+                    fontSize: 56,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      monthOfYear,
+                      style: TextStyle(
+                        color: themeData.textTheme.titleLarge?.color,
+                        fontSize: 20,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      year,
+                      style: TextStyle(
+                        color: themeData.textTheme.titleLarge?.color,
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                const SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  dayOfWeek,
+                  style: TextStyle(
+                    color: themeData.textTheme.titleLarge?.color,
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const Spacer(),
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
